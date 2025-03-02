d49293be0cd5eee16191363899ffadba540a86d7
AgCho <156483249+AgCho@users.noreply.github.com>
2025-03-02 00:36:14 -0500

    End of Section 7

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,17 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { importProvidersFrom } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { routes } from './app/app.routes';
-import { provideRouter } from '@angular/router';
-import { ToastrModule } from 'ngx-toastr';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    importProvidersFrom(HttpClientModule),
-    importProvidersFrom(ToastrModule.forRoot()),
-    provideAnimations(),
-    provideRouter(routes)
-  ]
-}).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch(err => console.error(err));
