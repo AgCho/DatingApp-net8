b91bc53364a2124e83d35fd4d18e85e30d97cb88
AgCho <156483249+AgCho@users.noreply.github.com>
2025-03-12 00:13:52 -0400

    End of Section 14

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -33,9 +33,8 @@ public class AppUser
     public required string Country { get; set; }
 
     public List<Photo> Photos { get; set; } = [];
-/* 
-    public int GetAge()
-    {
-        return DateOfBirth.CaculateAge();
-    } */
+
+    public List<UserLike> LikedByUsers { get; set;} = [];
+
+    public List<UserLike> LikedUsers { get; set;} = [];
 }
